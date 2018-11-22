92bba8398cc3b6ec11e3a61fe418b443633187f0
benjpell38 <45257448+benjpell38@users.noreply.github.com>
2018-11-22 10:40:57 +0100

    Update Blackjack Functions.py

--- Blackjack Functions.py
+++ Blackjack Functions.py
@@ -6,6 +6,10 @@
     °                                                                                                               °
     °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"""
 
+""" On importe les modules nécéssaires à nos différentes fonctions """
+
+from random import sample
+
 """" Il s'agit ici d'écrire la fonction paquet() qui permet de générer le paquet de 52 cartes nécessaire au jeu de Blackjack"""
 
 def paquet():
@@ -27,7 +31,7 @@ est associée (Dans notre exemple : 1) """
 def valeurCarte(carte):
     if '1 de' in carte:                                                                                                 # On filtre le cas où le joueur a un as
         value = int(input("Vous disposez d'un as, souhaitez-vous que sa valeur soit 1 ou 11 ? "))
-    for range_1 in range (2, 11, 1):                                                                                    # On filtre ensuite les cartes de 2  à 10
+    for range_1 in range(2, 11, 1):                                                                                    # On filtre ensuite les cartes de 2  à 10
         if str(range_1) in carte :
             value = range_1
     if 'valet' in carte or 'dame' in carte or 'roi' in carte:                                                           # On filtre les têtes
@@ -37,3 +41,12 @@ def valeurCarte(carte):
 
 """ Il s'agit ici d'écrire une fonction qui pour un entier n reçu renvoie une pioche constituée de n paquets de cartes
 avec n le nombre de joueurs de la partie """
+
+def initPioche(n):
+    pioche = []                                                                                                         # On crée une liste "Pioche" qui sera utilisée pour stocker les cartes
+    for range_1 in range(0,n, 1):                                                                                      # On crée une pioche contenant n paquets de cartes
+        for range_2 in range(0, len(paquet()), 1):
+            pioche.append(paquet()[range_2])
+    return sample(pioche, len(pioche))                                                                                  # On retourne une pioche triée aléatoirement
+
+
